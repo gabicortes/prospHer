7fe9826feb230a4166a4bd9f38916fa41e98443e
Gabi Cortes <g-cortes@hotmail.es>
2021-03-06 22:19:11 +0000

    menu bottom

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,17 +1,23 @@
 import React from "react";
-import InputGroup from "react-bootstrap/InputGroup"
-import FormControl from "react-bootstrap/FormControl"
-import Button from "react-bootstrap/Button"
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
-export function Chat (){
-    return (
-        <div>
-        <InputGroup className="mb-3">
-        <FormControl placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-            <InputGroup.Append>
-                <Button variant="outline-secondary">Button</Button>
-            </InputGroup.Append>
-        </InputGroup>
-        </div>
-    )
+export function Chat() {
+  return (
+    <div>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Button</Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </div>
+  );
+  
 }
