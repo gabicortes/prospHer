c8a23db5ce341b6abec002cc38f8faf64210a1e7
Gabi Cortes <g-cortes@hotmail.es>
2021-03-06 20:46:35 +0000

    third commit. sign in component done

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import "./App.css";
 import { Home } from "./Home/Home.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <div className="App">{/*    <Home /> */}
+  </div>;
 }
 
 export default App;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { NavBar } from "./NavBar/NavBar.js";
+import "./Home.css";
 
 export function Home() {
   return (
-    <div>
-      <NavBar />
+    <div className="homeWrapperLogin">
+      <div className="signUpTitle">Sign Up</div>
+      <input className="inputLogin" placeholder="Full Name" />
+      <input className="inputLogin" placeholder="Email" />
+      <input className="inputLogin" placeholder="Password" />
+      <div className="buttonSubmitNewUser">Create your account</div>
+      <div className="textBehindInputLogin">
+        By creating an account, you agree to our&nbsp;
+        <a className="linkSignUp" href="https://google.com.ar">
+          Terms
+        </a>
+      </div>
+      <div className="textBehindInputLogin">
+        Already have an Account? <a className="linkSignUp">Sign in</a>
+      </div>
     </div>
   );
 }
